41005c3bdd89bf07ba002b950e3d31956b953b01
Cameron Little <cameron@camlittle.com>
2015-04-22 17:57:00 -0700

    Initial Commit

---
+++ Watch Snap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watch Snap
+//
+//  Created by Cameron Little on 4/22/15.
+//  Copyright (c) 2015 Cameron Little. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
